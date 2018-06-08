e99d5dba7de72a23099cf0d44c99c24ebfb5fbaf
mcaceres2017 <39891148+mcaceres2017@users.noreply.github.com>
2018-06-08 15:03:32 -0400

    Add files via upload

--- batalla_naval.c
+++ batalla_naval.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct // vidas
+	{
+
+		int barcoA; //barco de 5
+		int barcoB;	//barco de 4
+		int barcoC;	//barco de 3
+		int barcoD; //barco de 3
+		int barcoE; //barco de 2
+	}barcosVivos;
+
+void RellenarMatriz(char *matriz)
+{
+	int i;
+	for(i=0;i<100;i++)
+	{
+		*(matriz+i)='.';
+	}
+}
+
 char obtenerBlanco(int n, char cordenadas[n], char *matriz)
 {	
 	int j,k;
@@ -9,7 +28,7 @@ char obtenerBlanco(int n, char cordenadas[n], char *matriz)
 
 	j=cordenadas[0]-'A';
 
-	for(k=1,k<n;k++)
+	for(k=1;k<n;k++)
 	{
 		i=i*10+(cordenadas[k]-'0');
 	}
@@ -31,7 +50,7 @@ void ingresarMatriz(char *matriz)//puntero tomado por la funcion
 	{
 		c=fgetc(stdin);//lee un caracter
 
-		if(c!='\n')//si el caracter no es salto de linea
+		if(c!='\n' )//si el caracter no es salto de linea
 		{
 			*(matriz+i)=c; // aquello apuntado por el puntero es igual al caracter ingresado
 			i++; //i sirve para desplazar a donde apunta el puntero en la matriz
@@ -44,32 +63,25 @@ int main()
 	char tablero_de_barcos1[10][10];
 	char tablero_de_barcos2[10][10]; //declaracion de los tableros de posiocion de barcos
 	char disparo[3];
+	
+	int tablero_de_juego1[10][10];
+	int tablero_de_juego2[10][10]; //declaracion  de los tableros visibles para los jugadores
 
-	int tablero_de_juego1[10][10]={0};
-	int tablero_de_juego2[10][10]={0}; //declaracion  de los tableros visibles para los jugadores
+	int i,j,c,aux,contador,p;
+	char *pt1=&tablero_de_barcos1[0];
+	char *pt2=&tablero_de_barcos2[0];
+	char *pt3=&tablero_de_juego1[0];
+	char *pt4=&tablero_de_juego2[0];
 
-	int i,j,c,aux,contador;
-	char *pt1;
-	char *pt2;
-
-	typedef struct // vidas
-	{
-
-		int barcoA; //barco de 5
-		int barcoB;	//barco de 4
-		int barcoC;	//barco de 3
-		int barcoD; //barco de 3
-		int barcoE; //barco de 2
-	}barcosVivos;
+	RellenarMatriz(pt3);
+	RellenarMatriz(pt4);
 
 	barcosVivos flota1={5,4,3,3,2}; //barcos del jugador 1
 	barcosVivos flota2={5,4,3,3,2}; //barcos del jugador 2
 
 	printf("jugador 1 ingrese la posiocion de sus barcos\n");
 
-	pt1=&tablero_de_barcos1[0][0];//el puntero 1 a punta a tablero_de_barcos1[0][0]
-	pt2=&tablero_de_barcos2[0][0]; //el puntero 2 a punta a tablero_de_barcos2[0][0]
-
+	
 	ingresarMatriz(pt1);
 
 	printf("jugador 2 ingrese la posiocion de sus barcos\n");
@@ -82,6 +94,9 @@ int main()
 
 	while(barcos_de_jugador_1>0 && barcos_de_jugador_2>0) // condicion del ternimo de juego
 	{
+		if (p==1)
+		{
+			
 		do
 		{
 			printf("jugador 1, dispare\n");
@@ -106,14 +121,51 @@ int main()
 					aux=1;
 				}
 
-				if(contador>0 && (c<'0' || c>'9')) // si es la segunda cordenada y la entrada no es un numero
+				if(contador==1 && (c<'0' || c>'9')) // si es la segunda cordenada y la entrada no es un numero
+				{
+					aux=1;
+				}
 
-				contador++;
+				else if (contador==1 && c=='1')
+				{
+					contador++;
+					disparo[contador]=c;
+				}
+				if (contador==2 && c!='0')
+				{
+					c=NULL;
+					disparo[contador]=c;
+				}
 			}
 
 		}while(aux==1);
 
 		cordenada_apuntada=obtenerBlanco(contador,disparo,pt1);
+		if (cordenada_apuntada='A')
+		{
+			
+		}
+		else if (cordenada_apuntada=='B')
+		{
+			
+		}
+		else if (cordenada_apuntada=='C')
+		{
+			
+		}
+		else if (cordenada_apuntada=='D')
+		{
+			
+		}
+		else if (cordenada_apuntada=='E')
+		{
+			
+		}
+		else 
+		{
+
+		}
+		p++;
 
 	}
 
